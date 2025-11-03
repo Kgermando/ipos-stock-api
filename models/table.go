1e5a852d12b058db1989084058014b5f982e6857
KGermando <katakugermain@gmail.com>
2025-11-03 16:29:41 +0100

    Add TeblaBox model with fields for order management and synchronization

---
+++ models/table.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type TeblaBox struct {
+	UUID      string `gorm:"type:varchar(255);primary_key" json:"uuid"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	PosUUID   string         `gorm:"type:varchar(255);not null" json:"pos_uuid"`
+	Pos       Pos            `gorm:"foreignKey:PosUUID;references:UUID"` // Point de vente
+
+	Ncommande string  `gorm:"not null" json:"ncommande"` // Number Random
+	Status    string  `json:"status"`                    // Ouverte et Fermée
+	TotalHt   float64 `gorm:"not null" json:"total_ht"`  // Total amount excluding tax
+	TotalTva  float64 `gorm:"not null" json:"total_tva"` // Total tax amount
+	TotalTtc  float64 `gorm:"not null" json:"total_ttc"` // Total amount including tax
+	ClientUUID     string `gorm:"type:varchar(255);not null" json:"client_uuid"`
+	Client         Client `gorm:"foreignKey:ClientUUID;references:UUID"` // Client
+	Signature      string `json:"signature"`
+	EntrepriseUUID string `json:"entreprise_uuid"`
+	Sync           bool   `gorm:"default:false" json:"sync"`
+
+	CommandeLines []CommandeLine `gorm:"foreignKey:CommandeUUID;references:UUID"` // Liste des lignes de commande
+
+	
+}
